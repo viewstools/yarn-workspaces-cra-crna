625057f043b595d6002f9f3d75901f05c47ca23a
Darío Javier Cravero <dario@uxtemple.com>
2017-12-28 09:59:05 +0000

    fix: add glob dependency and pass everything through glob
    Fixes #2.

--- get-yarn-workspaces/index.js
+++ get-yarn-workspaces/index.js
@@ -1,23 +1,15 @@
-const findRoot = require('find-root')
-const fs = require('fs')
-const path = require('path')
-const globa = require('glob')
+const findRoot = require('find-root');
+const flatten = require('flatten');
+const fs = require('fs');
+const path = require('path');
+const glob = require('glob');
 
 module.exports = function getWorkspaces(from) {
   const root = findRoot(from, dir => {
-    const pkg = path.join(dir, 'package.json')
-    return fs.existsSync(pkg) && 'workspaces' in require(pkg)
-  })
+    const pkg = path.join(dir, 'package.json');
+    return fs.existsSync(pkg) && 'workspaces' in require(pkg);
+  });
 
-  const { workspaces } = require(path.join(root, 'package.json'))
-  const paths = [].concat.apply(
-    [],
-    workspaces
-      .map(name => path.join(root, name))
-      .map(p => {
-        if (!p.match(/\*/)) return p
-        return glob.sync(p)
-      })
-  )
-  return paths
-}
+  const {workspaces} = require(path.join(root, 'package.json'));
+  return flatten(workspaces.map(name => glob.sync(path.join(root, name))));
+};
