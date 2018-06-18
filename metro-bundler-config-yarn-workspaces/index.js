03f2b2a53fb8ce8a165fa9f67972a34ebd0e2cd0
Ovidiu Cherecheș <skidding@gmail.com>
2018-06-18 20:34:41 +0300

    Add option for disabling Metro .babelrc lookup

--- metro-bundler-config-yarn-workspaces/index.js
+++ metro-bundler-config-yarn-workspaces/index.js
@@ -29,6 +29,10 @@ module.exports = function getConfig(from, options = {}) {
         options.nodeModules || path.resolve(from, '..', 'node_modules'),
       ].concat(workspaces)
     },
+    getEnableBabelRCLookup() {
+      // Whether Metro should pick up .babelrc configs from host project
+      return options.enableBabelRCLookup || true
+    }
   }
   return config
 }
